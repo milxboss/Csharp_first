82eabc1ac4286004dd71e7fa09c9b2eb94c460f9
ryx. <hokonvaglakxd@gmail.com>
2024-09-06 09:13:13 +0200

    argumentumok

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Csharp_first;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("why so serious?");
+        Console.WriteLine("Az első argumentum: "+ args[0]);
+
+        Console.Write("1. sor;\n");
+        System.Console.WriteLine("2. sor;");
+    }
+}
